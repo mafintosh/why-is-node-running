e10c1b79a80f3a6aada4c8b03e19c990482e5041
Karl von Randow <karl@xk72.com>
2019-04-03 07:23:56 +1300

    CLI can pass additional command-line args to child program

--- cli.js
+++ cli.js
@@ -4,6 +4,7 @@ var spawn = require('child_process').spawn
 var path = require('path')
 
 var prog = path.resolve(process.argv[2])
+var progArgs = process.argv.slice(3)
 
 console.log('probing program', prog)
 
@@ -12,6 +13,6 @@ var nodeArgs = [
   path.join(__dirname, 'include.js')
 ]
 var nodeOpts = { stdio: 'inherit' }
-var child = spawn('node', nodeArgs.concat(prog), nodeOpts)
+var child = spawn('node', nodeArgs.concat(prog).concat(progArgs), nodeOpts)
 
 console.log('kill -SIGUSR1', child.pid, 'for logging')
